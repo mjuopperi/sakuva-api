b2c13df71118ec26322171f504475aadbae92224
Markus Juopperi <markus@juopperi.io>
2023-01-01 20:28:01 +0200

    Deploy

---
+++ .github/scripts/deploy.sh
@@ -0,0 +1,16 @@
+# Setup ssh
+mkdir -p "$HOME"/.ssh
+ssh-keyscan "$SERVER_ADDRESS" >> "$HOME"/.ssh/known_hosts
+eval "$(ssh-agent)"
+ssh-add - <<< "$SSH_PRIVATE_KEY"
+
+# Update docker-compose
+envsubst < prod/docker-compose.template.yml > prod/docker-compose.yml
+scp prod/docker-compose.yml "$SSH_USERNAME@$SERVER_ADDRESS:/data/sakuva/docker-compose.yml"
+
+# Pull and restart changed
+ssh "$SSH_USERNAME@$SERVER_ADDRESS" "
+  cd /data/sakuva
+  sudo docker-compose pull
+  sudo docker-compose up -d
+"
