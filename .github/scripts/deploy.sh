5cc2f793e23ad04c58e50c16f645100b47a01966
Markus Juopperi <markus@juopperi.io>
2023-01-02 03:29:01 +0200

    Caddy

--- .github/scripts/deploy.sh
+++ .github/scripts/deploy.sh
@@ -8,9 +8,14 @@ ssh-add - <<< "$SSH_PRIVATE_KEY"
 envsubst < prod/docker-compose.template.yml > prod/docker-compose.yml
 scp prod/docker-compose.yml "$SSH_USERNAME@$SERVER_ADDRESS:/data/sakuva/docker-compose.yml"
 
+# Update caddy
+envsubst < prod/Caddyfile.template > prod/Caddyfile
+scp prod/Caddyfile "$SSH_USERNAME@$SERVER_ADDRESS:/data/sakuva/Caddyfile"
+
 # Pull and restart changed
 ssh "$SSH_USERNAME@$SERVER_ADDRESS" "
   cd /data/sakuva
   sudo docker-compose pull --quiet
   sudo docker-compose up -d
+  sudo docker exec sakuva_caddy_1 caddy reload --config /etc/caddy/Caddyfile
 "
